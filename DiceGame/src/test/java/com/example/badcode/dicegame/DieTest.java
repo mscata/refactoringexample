f0837328143012b17dc64ffb4270c7a91b328009
Marco Scata <mscata@hotmail.com>
2020-11-22 16:15:17 +0000

    Fixing test lifecycle

--- DiceGame/src/test/java/com/example/badcode/dicegame/DieTest.java
+++ DiceGame/src/test/java/com/example/badcode/dicegame/DieTest.java
@@ -1,6 +1,6 @@
 package com.example.badcode.dicegame;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
