f8cf2f373787ddd9b34885aed8299426881048ba
Richard Ackroyd <richard.ackroyd@live.co.uk>
2020-11-25 08:42:51 +0000

    TODO for exercise 1

--- DiceGame/src/test/java/com/example/badcode/dicegame/DieTest.java
+++ DiceGame/src/test/java/com/example/badcode/dicegame/DieTest.java
@@ -19,4 +19,8 @@ public class DieTest {
         die.roll();
         assertThat(die.getFaceValue(), is(not(0)));
     }
+
+    //TODO: 1 - Add a test to this class that tests the upper bound of the die roll.
+    //TODO: 2 - What are the problems with this test?  Make notes to share with the group any observations.
+    //TODO: 3 - What is the test actually testing?  Make notes to share with the group any observations.
 }
